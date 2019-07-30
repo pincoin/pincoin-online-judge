05f8da205efd1ec74f539a13fddab19c69989959
pincoin <pincoins@gmail.com>
2019-07-30 22:44:34 +0900

    - test

--- sandbox/tests.py
+++ sandbox/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SandboxTest(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_something_that_will_pass(self) -> None:
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self) -> None:
+        self.assertTrue(False)
