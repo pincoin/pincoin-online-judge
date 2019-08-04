1eb82f36ee74dd7f29dcffb15e3276cbacc1d411
pincoin <pincoins@gmail.com>
2019-08-04 12:38:58 +0900

    - member app added

--- member/apps.py
+++ member/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class QuestConfig(AppConfig):
-    name = 'quest'
-    verbose_name = _('Quest App')
+class MemberConfig(AppConfig):
+    name = 'member'
+    verbose_name = _('Member App')
