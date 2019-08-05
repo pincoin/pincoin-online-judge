ee3397778c558053921c3fdf93b8adc782e57f8c
pincoin <pincoins@gmail.com>
2019-08-05 21:12:21 +0900

    - base layout

--- conf/static/conf/js/base.js
+++ conf/static/conf/js/base.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     $('.dropdown-trigger').dropdown();
+
     $('.sidenav').sidenav();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true,
+    });
+
+    setInterval(function () {
+        $('.carousel.carousel-slider').carousel('next');
+    }, 3000);
 });
