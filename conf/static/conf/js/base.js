52a29a9d2c5a6d0b00a194142a218a641f61108d
pincoin <pincoins@gmail.com>
2019-08-06 15:09:03 +0900

    - favicon

--- conf/static/conf/js/base.js
+++ conf/static/conf/js/base.js
@@ -7,8 +7,4 @@ $(document).ready(function () {
         fullWidth: true,
         indicators: true,
     });
-
-    setInterval(function () {
-        $('.carousel.carousel-slider').carousel('next');
-    }, 3000);
 });
