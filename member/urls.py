7e9e5e12ecf9778d2c8b3472f1bb01628416a427
pincoin <pincoins@gmail.com>
2019-08-10 02:44:45 +0900

    - facebook login

--- member/urls.py
+++ member/urls.py
@@ -1,3 +1,6 @@
+from importlib import import_module
+
+from allauth.socialaccount import providers
 from django.urls import (
     path, re_path
 )
@@ -59,3 +62,13 @@ urlpatterns = [
     path('social/connections/',
          views.MemberSocialConnectionsView.as_view(), name='socialaccount_connections'),
 ]
+
+# URL patterns for social providers
+for provider in providers.registry.get_list():
+    try:
+        prov_mod = import_module(provider.get_package() + '.urls')
+    except ImportError:
+        continue
+    prov_urlpatterns = getattr(prov_mod, 'urlpatterns', None)
+    if prov_urlpatterns:
+        urlpatterns += prov_urlpatterns
