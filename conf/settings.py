9ab733574e584d5abfbccd48ac3c82eb461ff69e
pincoin <pincoins@gmail.com>
2019-08-10 22:36:01 +0900

    - line provider custom

--- conf/settings.py
+++ conf/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS += [
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.line',
+    'member.socialaccount.providers.line',
 ]
 
 '''
