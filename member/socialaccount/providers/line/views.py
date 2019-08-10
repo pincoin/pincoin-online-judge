3f19ebc2fc27ca332b41416cd89c820b2f9a6ea2
pincoin <pincoins@gmail.com>
2019-08-10 23:09:09 +0900

    - jwt decoded email

--- member/socialaccount/providers/line/views.py
+++ member/socialaccount/providers/line/views.py
@@ -1,6 +1,5 @@
-import requests
 import jwt
-
+import requests
 from allauth.socialaccount.providers.oauth2.views import (
     OAuth2Adapter,
     OAuth2CallbackView,
@@ -16,8 +15,20 @@ class LineOAuth2Adapter(OAuth2Adapter):
     authorize_url = 'https://access.line.me/oauth2/v2.1/authorize'
     profile_url = 'https://api.line.me/v2/profile'
 
+    decoded_email = ''
+
     def parse_token(self, data):
         token = super(LineOAuth2Adapter, self).parse_token(data)
+
+        id_token = data.get('id_token', '')
+
+        decoded_id_token = jwt.decode(id_token,
+                                      'df126489de181c230ba80f4b446e727c',
+                                      audience='1607852693',
+                                      issuer='https://access.line.me',
+                                      algorithms=['HS256'])
+
+        self.decoded_email = decoded_id_token['email']
 
         return token
 
@@ -25,6 +36,7 @@ class LineOAuth2Adapter(OAuth2Adapter):
         headers = {'Authorization': 'Bearer {0}'.format(token.token)}
         resp = requests.get(self.profile_url, headers=headers)
         extra_data = resp.json()
+        extra_data['email'] = self.decoded_email
         return self.get_provider().sociallogin_from_response(request,
                                                              extra_data)
 
