7e0d3a76500706b3d4deaedd965d718df248bb1b
pincoin <pincoins@gmail.com>
2019-08-12 21:39:27 +0900

    - notify password changed

--- member/signals.py
+++ member/signals.py
@@ -1,4 +1,6 @@
-from allauth.account.signals import user_logged_in
+from allauth.account.signals import (
+    user_logged_in, password_changed, password_reset, password_set
+)
 from django.dispatch import receiver
 from ipware.ip import get_ip
 
@@ -11,3 +13,18 @@ def login_logger(request, user, **kwargs):
     login_log.user = user
     login_log.ip_address = get_ip(request)
     login_log.save()
+
+
+@receiver(password_changed)
+def notify_password_changed(request, user, **kwargs):
+    pass
+
+
+@receiver(password_reset)
+def notify_password_reset(request, user, **kwargs):
+    pass
+
+
+@receiver(password_set)
+def notify_password_set(request, user, **kwargs):
+    pass
