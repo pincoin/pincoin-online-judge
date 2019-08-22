a88e7e3d270df0e8783f9c37c679d86f3a5dae94
pincoin <pincoins@gmail.com>
2019-08-22 13:07:51 +0900

    - password set / reset

--- member/forms.py
+++ member/forms.py
@@ -30,6 +30,11 @@ class MemberLoginForm(GoogleRecaptchaMixin, allauth_forms.LoginForm):
 
 
 class MemberResetPasswordForm(GoogleRecaptchaMixin, allauth_forms.ResetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super(MemberResetPasswordForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['class'] = 'input'
+
     def clean(self):
         self.validate_google_recaptcha()
 
@@ -42,6 +47,13 @@ class MemberResetPasswordKeyForm(allauth_forms.ResetPasswordKeyForm):
 
 
 class MemberChangePasswordForm(GoogleRecaptchaMixin, allauth_forms.ChangePasswordForm):
+    def __init__(self, *args, **kwargs):
+        super(MemberChangePasswordForm, self).__init__(*args, **kwargs)
+
+        self.fields['oldpassword'].widget.attrs['class'] = 'input'
+        self.fields['password1'].widget.attrs['class'] = 'input'
+        self.fields['password2'].widget.attrs['class'] = 'input'
+
     def clean(self):
         self.validate_google_recaptcha()
 
@@ -50,6 +62,12 @@ class MemberChangePasswordForm(GoogleRecaptchaMixin, allauth_forms.ChangePasswor
 
 
 class MemberSetPasswordForm(GoogleRecaptchaMixin, allauth_forms.SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super(MemberSetPasswordForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].widget.attrs['class'] = 'input'
+        self.fields['password2'].widget.attrs['class'] = 'input'
+
     def clean(self):
         self.validate_google_recaptcha()
 
